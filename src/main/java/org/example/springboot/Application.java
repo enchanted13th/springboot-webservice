b0daf1ca8389109a5452e3ef415aa983b4892d7b
ine <enchanted13th@gmail.com>
2020-08-08 01:09:54 +0900

    Add Chapter 02

---
+++ src/main/java/org/example/springboot/Application.java
@@ -0,0 +1,12 @@
+package org.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 프로젝트 최상단에 위치해야 한다.
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
