d5529d384a10452ba5e1564551dc9644673f2de8
ine <enchanted13th@gmail.com>
2020-08-25 14:16:40 +0900

    Add Chapter 03

--- src/main/java/org/example/springboot/Application.java
+++ src/main/java/org/example/springboot/Application.java
@@ -2,8 +2,10 @@ package org.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // 프로젝트 최상단에 위치해야 한다.
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
